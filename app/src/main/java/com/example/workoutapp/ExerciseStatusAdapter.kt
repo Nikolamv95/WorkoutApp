e1fe67477f790412638bf2d2d35e2cf9eef482fb
Nikola Margaritov <nikola.margaritoov@gmail.com>
2021-07-03 15:52:05 +0300

    Refactoring ExerciseStatusAdapter

--- app/src/main/java/com/example/workoutapp/ExerciseStatusAdapter.kt
+++ app/src/main/java/com/example/workoutapp/ExerciseStatusAdapter.kt
@@ -29,14 +29,14 @@ class ExerciseStatusAdapter (val items:ArrayList<ExerciseModel>, val context: Co
     // Here we adjust the view of every single element of the list of items
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val model: ExerciseModel = items[position]
-        holder.tvItem.text = model.getId().toString()
+        holder.tvItem.text = model.id.toString()
 
-        if (model.getIsSelected()){
+        if (model.isSelected){
             holder.tvItem.background =
                 ContextCompat.getDrawable(context, R.drawable.item_circular_thin_color_accent_border)
 
             holder.tvItem.setTextColor(Color.parseColor("#212121"))
-        } else if (model.getIsCompleted()){
+        } else if (model.isCompleted){
             holder.tvItem.background =
                 ContextCompat.getDrawable(context, R.drawable.item_circular_color_accent_background)
 
